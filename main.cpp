12e8a39b8f3719abfc28431c921df4b8a345afb0
land1and <160421248+land1and@users.noreply.github.com>
2025-05-12 04:12:14 +0100

    faster !

--- main.cpp
+++ main.cpp
@@ -45,29 +45,37 @@ void save_bmp(const uint8_t* grid, const uint8_t* palette, const std::string& fi
 void ant_thread(const uint64_t start, const uint64_t end) {
     std::mt19937 gen = std::mt19937{ std::random_device{}() };
     std::uniform_int_distribution<> dist(0, 255);
+    uint32_t size_minus_one = 0;
+    uint32_t size = 0;
+    uint32_t size_4_1 = 0;
+    uint8_t* pattern = new uint8_t[64];
+    uint8_t* palette = new uint8_t[256];
+    uint8_t* grid = new uint8_t[GRID_SQUARED]();
+    uint32_t index = 0;
+    uint32_t ant_position_x = 0;
+    uint32_t ant_position_y = 0;
+    int32_t ant_direction = 0;
+    uint32_t state = 0;
     for (uint64_t i = start; i < end; ++i) {
         if (!((i + 1) & i)) continue;
-        uint8_t size_minus_one = 0;
         for (int8_t j = 63; j >= 0; --j) {
             if (!(i & (1ULL << j))) continue;
             size_minus_one = j;
             break;
         }
-        uint8_t size = size_minus_one + 1;
-        uint8_t* pattern = new uint8_t[size];
-        for (uint8_t j = 0; j < size; ++j) {
+        size = size_minus_one + 1;
+        for (uint32_t j = 0; j < size; ++j) {
             pattern[INVERT_PATTERN ? j : size_minus_one - j] = ((i >> j) & 1) ^ INVERT_PATTERN;
         }
-        uint8_t* palette = new uint8_t[256];
-        for (uint8_t j = 0; j < ((size * 4) - 1); ++j) {
+        size_4_1 = (size * 4) - 1;
+        for (uint32_t j = 0; j < size_4_1; ++j) {
             palette[j] = dist(gen);
         }
-        uint8_t* grid = new uint8_t[GRID_SQUARED]();
-        uint32_t index = GRID_INDEX;
-        uint32_t ant_position_x = GRID_SIZE_HALF;
-        uint32_t ant_position_y = GRID_SIZE_HALF;
-        int32_t ant_direction = 1;
-        uint32_t state = 0;
+        index = GRID_INDEX;
+        ant_position_x = GRID_SIZE_HALF;
+        ant_position_y = GRID_SIZE_HALF;
+        ant_direction = 1;
+        state = 0;
         for (uint64_t j = 0; j < MAX_ITERATIONS; j += 2) {
             state = grid[index];
             grid[index] = state < size_minus_one ? state + 1 : 0;
@@ -82,23 +90,24 @@ void ant_thread(const uint64_t start, const uint64_t end) {
             ant_position_y += ant_direction;
             if (ant_position_y >= GRID_SIZE) break;
         }
+        std::memset(grid, 0, GRID_SQUARED);
         save_bmp(grid, palette, std::to_string(i) + ".bmp");
-        delete[] pattern;
-        delete[] palette;
-        delete[] grid;
     }
+    delete[] pattern;
+    delete[] palette;
+    delete[] grid;
 }
 
 int main() {
-    uint64_t start_pattern = std::max<uint64_t>(START_PATTERN, 0);
-    uint64_t num_patterns = std::max<uint64_t>(NUM_PATTERNS, 1);
-    uint64_t num_threads = std::min<uint64_t>(std::max<uint64_t>(NUM_THREADS < 0 ? std::thread::hardware_concurrency() : NUM_THREADS, 1), num_patterns);
-    uint64_t patterns_per_thread = num_patterns / num_threads;
-    uint64_t remainder = num_patterns % num_threads;
+    const uint64_t start_pattern = std::max<uint64_t>(START_PATTERN, 0);
+    const uint64_t num_patterns = std::max<uint64_t>(NUM_PATTERNS, 1);
+    const uint64_t num_threads = std::min<uint64_t>(std::max<uint64_t>(NUM_THREADS < 0 ? std::thread::hardware_concurrency() : NUM_THREADS, 1), num_patterns);
+    const uint64_t patterns_per_thread = num_patterns / num_threads;
+    const uint64_t remainder = num_patterns % num_threads;
     std::vector<std::thread> threads;
     for (uint64_t i = 0; i < num_threads; ++i) {
-        uint64_t start = start_pattern + (i * patterns_per_thread) + std::min<uint64_t>(i, remainder);
-        uint64_t end = start + patterns_per_thread + (i < remainder);
+        const uint64_t start = start_pattern + (i * patterns_per_thread) + std::min<uint64_t>(i, remainder);
+        const uint64_t end = start + patterns_per_thread + (i < remainder);
         threads.emplace_back(ant_thread, start, end);
     }
     for (std::thread& thread : threads) {
